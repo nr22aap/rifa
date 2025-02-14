12345e3ba26724db75a751d7765bf21894561de6
nr22aap <nr22aap@herts.ac.uk>
2025-02-14 19:11:50 +0000

    Added navbar.php for navigation structure across pages

---
+++ includes/navbar.php
@@ -0,0 +1,9 @@
+<nav>
+    <ul>
+        <li><a href="/fyp_hms/pages/index.php">Dashboard</a></li>
+        <li><a href="/fyp_hms/pages/admin_login.php">Administration</a></li>
+        <li><a href="/fyp_hms/pages/login.php">Doctor's Portal</a></li>
+        <li><a href="/fyp_hms/pages/location.php">Location</a></li>
+        <li><a href="/fyp_hms/pages/about_us.php">About Us</a></li>
+    </ul>
+</nav>
