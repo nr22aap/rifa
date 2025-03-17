542f0b99f1335ccb4b75059c6f7596152b9207bc
nr22aap <nr22aap@herts.ac.uk>
2025-03-17 01:41:23 +0000

    Updated the code

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,14 +1,13 @@
 <?php
 $host = 'localhost';
-$db = 'hms_database'; // Updated database name
-$user = 'root'; // Default XAMPP username
-$pass = '';     // Default XAMPP password (blank)
+$db = 'hms_database'; 
+$user = 'root'; 
+$pass = '';     
 
 try {
-    // Establish a connection to the database
     $conn = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // echo "Database connected successfully!";
+    
 
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
