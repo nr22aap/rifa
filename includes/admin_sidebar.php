9dfd4d87ca9fbdf105d192521b25dd1cd95c4140
nr22aap <nr22aap@herts.ac.uk>
2025-03-24 02:38:26 +0000

    Updated the admin sidebar

---
+++ includes/admin_sidebar.php
@@ -0,0 +1,12 @@
+<div class="sidebar">
+    <h2>Admin Panel</h2>
+    <ul>
+        <li><a href="../pages/admin_dashboard.php"><i class="fas fa-home"></i> Dashboard</a></li>
+        <li><a href="../pages/manage_doctors.php"><i class="fas fa-user-md"></i> Manage Doctors</a></li>
+        <li><a href="../pages/manage_patients.php"><i class="fas fa-procedures"></i> Manage Patients</a></li>
+        <li><a href="../pages/prescriptions.php"><i class="fas fa-prescription"></i> Prescriptions</a></li>
+        <li><a href="../pages/lab_tests.php"><i class="fas fa-vials"></i> Lab Tests</a></li>
+        <li><a href="../pages/settings.php"><i class="fas fa-cog"></i> Settings</a></li>
+        <li><a href="../backend/admin_logout.php" class="logout-btn"><i class="fas fa-sign-out-alt"></i> Logout</a></li>
+    </ul>
+</div>
